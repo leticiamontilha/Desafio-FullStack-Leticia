51477a26a87b319406483559954fee9184752dd5
leticiamontilha <leticiammontilha@hotmail.com>
2023-05-24 17:21:15 -0300

    fix: bugs import]

--- dist/middlewares/verifyData.middleware.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const dataIsValidMiddleware = (schema) => (request, response, next) => {
-    const validatedData = schema.parse(request.body);
-    request.body = validatedData;
-    return next();
-};
-exports.default = dataIsValidMiddleware;
